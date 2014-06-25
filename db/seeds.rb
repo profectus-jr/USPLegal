5f56f49a3188d7274317cd676270c20af9cc970e
root <root@ubuntu.(none)>
2014-06-25 13:11:19 -0300

    from server

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name: "Administrador", nusp: 1234567, cpf: "123.456.789-10", password: "password", email: "administrador@usp.legal.br", kind: "admin")
-estag = User.create(name: "Mariana da Silva Gonçalves", nusp: 8555101, cpf: "123.456.789-10", password: "password", email: "estagiario@usp.legal.br", kind: "estag")
-admin = User.create(name: "Lucia Filgueiras", nusp: 52238, cpf: "123.456.789-10", password: "12345678", email: "admin@usp.br", kind: "admin")
-
 passeio_publico = EquipType.create(kind: "PASSEIO PÚBLICO")
 ChecklistItem.create(question: "O equipamento está conservado? ", equip_type: passeio_publico)
 ChecklistItem.create(question: "A faixa livre possui largura livre mínima de 1,20 m?", equip_type: passeio_publico)
