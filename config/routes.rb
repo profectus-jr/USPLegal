16b18dfc4b5e546c216bbe9e5c398d90ff2478c2
Daniel Barbosa Stein <ksatechnology@gmail.com>
2015-08-10 15:55:57 -0300

    Config

--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,8 @@ Hera::Application.routes.draw do
   resources :equip_types
 
   get "gerenciamento/index"
+  
+  get "configuration/index"
 
     # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
